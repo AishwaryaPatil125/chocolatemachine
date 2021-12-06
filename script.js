d5dc1f69a4cbd43d1b06b3ff95faff643e3bf58d
AishwaryaPatil125 <89911139+AishwaryaPatil125@users.noreply.github.com>
2021-12-06 23:36:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,169 @@
+function printChocolate(name){	
+	console.log(name);
+	if(name=="kitkat"){
+		var rate=100;
+		var quantity=document.getElementById("name1").value;
+		var total=quantity*rate;
+		console.log(total);
+		document.getElementById("print1").innerHTML=total;
+	    
+	}
+	
+	if(name=="Milkybar"){
+		var rate=120;
+		var quantity=document.getElementById("name2").value;
+		var total=quantity*rate;
+		document.getElementById("print2").innerHTML=total;
+	    
+	}
+	
+	if(name=="Dairymilk"){
+		var rate=150;
+		var quantity=document.getElementById("name3").value;
+		var total=quantity*rate;
+		document.getElementById("print3").innerHTML=total;
+	    
+	}
+	
+	if(name=="Perk"){
+		var rate=190;
+		var quantity=document.getElementById("name4").value;
+		var total=quantity*rate;
+		document.getElementById("print4").innerHTML=total;
+	    
+	}
+	
+	if(name=="Temptations"){
+		var rate=250;
+		var quantity=document.getElementById("name5").value;
+		var total=quantity*rate;
+		document.getElementById("print5").innerHTML=total;
+	}
+	
+	if(name=="Luvit"){
+		var rate=180;
+		var quantity=document.getElementById("name6").value;
+		var total=quantity*rate;
+		document.getElementById("print6").innerHTML=total;
+	    
+	}
+	
+	if(name=="Munch"){
+		var rate=160;
+		var quantity=document.getElementById("name7").value;
+		var total=quantity*rate;
+		document.getElementById("print7").innerHTML=total;
+	    
+	}
+	
+	if(name=="Eclairs"){
+		var rate=50;
+		var quantity=document.getElementById("name8").value;
+		var total=quantity*rate;
+		document.getElementById("print8").innerHTML=total;
+	    
+	}
+	
+	if(name=="Butterfun"){
+		var rate=80;
+		var quantity=document.getElementById("name9").value;
+		var total=quantity*rate;
+		document.getElementById("print9").innerHTML=total;
+	    
+	}
+	
+	if(name=="Alpenliebe"){
+		var rate=100;
+		var quantity=document.getElementById("name10").value;
+		var total=quantity*rate;
+		document.getElementById("print10").innerHTML=total;
+	    
+	}
+	
+	if(name=="Lolipop"){
+		var rate=200;
+		var quantity=document.getElementById("name11").value;
+		var total=quantity*rate;
+		document.getElementById("print11").innerHTML=total;
+	   
+	}
+	
+	if(name=="Bueno"){
+		var rate=500;
+		var quantity=document.getElementById("name12").value;
+		var total=quantity*rate;
+		document.getElementById("print12").innerHTML=total;
+	    
+	}
+	
+	if(name=="Aasay"){
+		var rate=1000;
+		var quantity=document.getElementById("name13").value;
+		var total=quantity*rate;
+		document.getElementById("print13").innerHTML=total;
+	    
+	}
+	
+	if(name=="Greenapple"){
+		var rate=500;
+		var quantity=document.getElementById("name14").value;
+		var total=quantity*rate;
+		document.getElementById("print14").innerHTML=total;
+	    
+	}
+	
+	if(name=="Snickers"){
+		var rate=600;
+		var quantity=document.getElementById("name15").value;
+		var total=quantity*rate;
+		document.getElementById("print15").innerHTML=total;
+	    
+	}
+	
+	if(name=="Darkchocolate"){
+		var rate=1000;
+		var quantity=document.getElementById("name16").value;
+		var total=quantity*rate;
+		document.getElementById("print16").innerHTML=total;
+	    
+	}
+	
+	if(name=="Bournville"){
+		var rate=2000;
+		var quantity=document.getElementById("name17").value;
+		var total=quantity*rate;
+		document.getElementById("print17").innerHTML=total;
+	   
+	}
+	
+	if(name=="Melto"){
+		var rate=1500;
+		var quantity=document.getElementById("name18").value;
+		var total=quantity*rate;
+		document.getElementById("print18").innerHTML=total;
+	    
+	}
+	
+	if(name=="Fivestar"){
+		var rate=3000;
+		var quantity=document.getElementById("name19").value;
+		var total=quantity*rate;
+		document.getElementById("print19").innerHTML=total;
+	    
+	}
+	
+	if(name=="Galaxy"){
+		var rate=2500;
+		var quantity=document.getElementById("name20").value;
+		var total=quantity*rate;
+		document.getElementById("print20").innerHTML=total;
+	    
+	}
+	
+	if(name=="Hersheys"){
+		var rate=1600;
+		var quantity=document.getElementById("name21").value;
+		var total=quantity*rate;
+		document.getElementById("print21").innerHTML=total;
+	}
+}
